2fb9d94189a56dfcfd053464970157c9e7a61a9e
hugues0 <ntwari.hugues@gmail.com>
2022-02-12 13:01:48 +0200

    ch:moved config seed data  into .env file

--- database/seeders/ConfigurationSeeder.php
+++ database/seeders/ConfigurationSeeder.php
@@ -18,10 +18,11 @@ class ConfigurationSeeder extends Seeder
     {
         \DB::table('configurations')->truncate();
 
-      Configuration::create(['name' => 'vat', 'value' => 0.18]);
-      Configuration::create(['name' => 'discount', 'value' => 0.2]);
-      Configuration::create(['name' => 'transaction_fee', 'value' => 100]); 
-      Configuration::create(['name' => 'coupon', 'value' => 0.02]); 
+      Configuration::create(['name' => 'vat', 'value' => env('VAT')]);
+      Configuration::create(['name' => 'discount', 'value' => env('DISCOUNT')]);
+      Configuration::create(['name' => 'transaction_fee','value' => env('TRANSACTION_FEE')]); 
+      Configuration::create(['name' => 'coupon', 'value' => env('COUPON_VALUE')]);
+      Configuration::create(['name' => 'points_value', 'value' => env('POINTS_VALUE')]);
 
     }
 }
